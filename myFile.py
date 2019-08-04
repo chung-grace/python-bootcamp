e6fec15040c4d12a3dbed8634edd207f10963922
Grace Chung <grace_chung@berkeley.edu>
2019-08-04 15:20:20 -0700

    added myFile.py

--- myFile.py
+++ myFile.py
@@ -1,1 +1,17 @@
-print("This is my file")
+# aList = [1, 2, 3, "pop", "sunday"]
+
+# for item in aList:
+#     print(item)
+#     if item == "sunday":
+#         print("yay")
+
+import sys
+
+def main():
+    print(sys.argv, type(sys.argv))
+    print(len(sys.argv))
+    print(sys.argv[0])
+    print(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
